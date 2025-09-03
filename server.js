33731775a40248eb8b6bc5e23d3a5906fa149283
Alexandre Henrique Nolla <alexandre.nolla@gmail.com>
2025-09-03 16:52:04 -0300

    update data

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "8017437143:AAEGpkrLgXLW5tpoMsx8MAUylJlQhRf-S3Q";
+const TELEGRAM_CHAT_ID = "-4979284883"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
